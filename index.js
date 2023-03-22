dde49bc00a13f58c8313c8cd5d7838adb9f254a5
Archy_k <artbilov@gmail.com>
2023-03-22 10:39:55 -0700

    create server

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-console.log(2+2);
+const {createServer} = require("http");
+
+const server = createServer(handleRequest);
+const port = process.env.PORT || 3000;
+
+server.listen(port, () => console.log("Server started at http://localhost:" + port)) ;
+
+function handleRequest(request, response) {
+    response.end("Hello world");
+}
