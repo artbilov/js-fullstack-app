a46575f3368a97a03da6f9088b83d5e56ce34405
Archy_k <artbilov@gmail.com>
2023-03-22 12:05:12 -0700

    Serve Files

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const { readFile } = require("fs").promises;
 const {createServer} = require("http");
 
 const server = createServer(handleRequest);
@@ -5,6 +6,17 @@ const port = process.env.PORT || 3000;
 
 server.listen(port, () => console.log("Server started at http://localhost:" + port)) ;
 
-function handleRequest(request, response) {
-    response.end("Hello world");
+async function handleRequest(request, response) {
+    console.log(request.url);
+
+    try {
+        const path = request.url.slice(1) || "index.html";
+        const fileContent = await readFile("public/" + path);
+        
+        response.end(fileContent);
+    } catch (error) {
+        response.statusCode = 404;
+        response.end("file not found " + request.url);
+    }
+    
 }
