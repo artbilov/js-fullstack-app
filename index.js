56e50b4029910ce2be66a50179c5b0feda72fece
Archy_k <artbilov@gmail.com>
2023-03-23 09:27:21 -0700

    Add CSS MIME type

--- index.js
+++ index.js
@@ -1,22 +1,26 @@
-const { readFile } = require("fs").promises;
-const {createServer} = require("http");
+const { readFile } = require('fs').promises;
+const {createServer} = require('http');
 
 const server = createServer(handleRequest);
 const port = process.env.PORT || 3000;
 
-server.listen(port, () => console.log("Server started at http://localhost:" + port)) ;
+server.listen(port, () => console.log('Server started at http://localhost:' + port)) ;
 
 async function handleRequest(request, response) {
     console.log(request.url);
 
     try {
-        const path = request.url.slice(1) || "index.html";
-        const fileContent = await readFile("public/" + path);
+        const path = request.url.slice(1) || 'index.html';
+        const fileContent = await readFile('public/' + path);
         
+        if (path.endsWith('.css')) {
+            response.setHeader('content-type', 'text/css; charset=UTF-8')
+        }
+
         response.end(fileContent);
     } catch (error) {
         response.statusCode = 404;
-        response.end("file not found " + request.url);
+        response.end('file not found ' + request.url);
     }
     
 }
