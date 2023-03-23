7eeb0900a0e25f17c9f2591c1a4c6192d74007d9
Archy_k <artbilov@gmail.com>
2023-03-23 11:06:39 -0700

    Handle API files

--- index.js
+++ index.js
@@ -1,26 +1,35 @@
 const { readFile } = require('fs').promises;
-const {createServer} = require('http');
+const { createServer } = require('http');
 
 const server = createServer(handleRequest);
 const port = process.env.PORT || 3000;
+const files = ['package.json'];
 
-server.listen(port, () => console.log('Server started at http://localhost:' + port)) ;
+server.listen(port, () => console.log('Server started at http://localhost:' + port));
 
 async function handleRequest(request, response) {
-    console.log(request.url);
+  console.log(request.url);
 
+  if (request.url.startsWith('/api/')) {
+    response.end(JSON.stringify(files))
+  } else {
     try {
-        const path = request.url.slice(1) || 'index.html';
-        const fileContent = await readFile('public/' + path);
-        
-        if (path.endsWith('.css')) {
-            response.setHeader('content-type', 'text/css; charset=UTF-8')
-        }
-
-        response.end(fileContent);
+      const path = request.url.slice(1) || 'index.html';
+      const fileContent = await readFile('public/' + path);
+  
+      if (path.endsWith('.css')) response.setHeader('content-type', 'text/css; charset=UTF-8')
+  
+      response.end(fileContent);
     } catch (error) {
-        response.statusCode = 404;
-        response.end('file not found ' + request.url);
+      response.statusCode = 404;
+      response.end('file not found ' + request.url);
     }
-    
-}
+
+  }
+
+  
+
+  
+} 
+
+global.files = files
--- public/script.js
+++ public/script.js
@@ -1,1 +1,18 @@
+getFileData().then(showFiles)
+
 document.body.append(document.createElement("input"));
+
+function getFileData() {
+  return fetch('/api/files').then(responce => responce.json())
+}
+
+function showFiles(files) {
+  const fileList = document.getElementById('file-list')
+
+  for (let i = 0; i < files.length; i++) {
+    const li = document.createElement('li')
+
+    li.append(files[i])
+    fileList.append(li)
+  }
+}
